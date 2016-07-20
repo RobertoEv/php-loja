d57eed82e94bb44498fca4228ef26b9cfb901183
Roberto <betoesf@gmail.com>
2016-07-20 06:07:23 -0300

    Add new field, category. Fixing XAMPP version in notebook adicionando produto-formulario atualizado

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,4 +1,9 @@
-<?php include("cabecalho.php"); ?>
+<?php include("cabecalho.php");
+include("conecta.php");
+include ("banco-categoria.php");
+
+$categorias = listaCategorias($conexao);
+?>
     <h1>Formulário de Produto</h1>
     <form action="adiciona-produto.php" method="post">
         <table class="table">
@@ -15,6 +20,16 @@
             <tr>
                 <td>Descrição</td>
                 <td><textarea class="form-control" name="descricao"></textarea></td>
+            </tr>
+
+            <tr>
+                <td>Categoria</td>
+                <td>
+                    <?php foreach ($categorias as $categoria) :?>
+                        <input type="radio" name="categoria_id" value="<?=$categoria['id']?>">
+                        <?=$categoria['nome']?>
+                    <?php endforeach;?>
+                </td>
             </tr>
 
             <tr>
