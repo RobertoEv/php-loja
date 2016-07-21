cce94415ab8833d3c78bfa664710d44989638543
Roberto <betoesf@gmail.com>
2016-07-20 22:13:55 -0300

    Adicionado Sobre e Ajustando o arquivo de conexão

--- conecta.php
+++ conecta.php
@@ -1,2 +1,3 @@
 <?php
-$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+//$conexao = mysqli_connect('mysql.hostinger.com.br', 'u141846900_loja', 'loja303134', 'u141846900_loja');
+$conexao = mysqli_connect('localhost', 'loja', '', 'loja');
---
+++ sobre.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roberto
+ * Date: 20/07/2016
+ * Time: 18:53
+ */
+
+include ("cabecalho.php");?>
+    <h1>Primeiro projeto de php!</h1>
+    <p>Primeiro projeto de php concluido, agora tentarei melhorar ainda mais :D</p>
+<?php include ("rodape.php"); ?>
